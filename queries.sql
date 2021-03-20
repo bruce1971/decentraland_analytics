2ca259653b7d827e18dc1e8b19468d28cfeeb1ca
bruch1971 <tropsyv3@gmail.com>
2021-03-20 23:27:05 +0100

    fix: queries

--- queries.sql
+++ queries.sql
@@ -4,9 +4,10 @@ SELECT
           			COUNT(1) AS sales,
                 ROUND(AVG(size), 2) as avg_size,
           			ROUND(AVG(price_usd_parcel)) AS avg_price_usd_parcel,
-          			ROUND(AVG(price_eth_parcel),3) AS avg_price_eth_parcel,
+          			ROUND(AVG(price_eth_parcel), 2) AS avg_price_eth_parcel,
           			ROUND(AVG(price_mana_parcel)) AS avg_price_mana_parcel
 FROM 		        sales
+WHERE           price_usd_parcel > 50
 GROUP BY 	      1
 ORDER BY  	    1 asc
 
@@ -17,9 +18,10 @@ SELECT
           			COUNT(1) AS sales,
           			ROUND(AVG(size), 2) as avg_size,
           			ROUND(AVG(price_usd_parcel)) AS avg_price_usd_parcel,
-          			ROUND(AVG(price_eth_parcel),3) AS avg_price_eth_parcel,
+          			ROUND(AVG(price_eth_parcel), 2) AS avg_price_eth_parcel,
           			ROUND(AVG(price_mana_parcel)) AS avg_price_mana_parcel
 FROM 		        sales
+WHERE           price_usd_parcel > 50
 GROUP BY 	      1
 ORDER BY  	    1 asc
 
@@ -30,8 +32,9 @@ SELECT
           			COUNT(1) AS sales,
           			ROUND(AVG(size), 2) as avg_size,
           			ROUND(AVG(price_usd_parcel)) AS avg_price_usd_parcel,
-          			ROUND(AVG(price_eth_parcel),3) AS avg_price_eth_parcel,
+          			ROUND(AVG(price_eth_parcel), 2) AS avg_price_eth_parcel,
           			ROUND(AVG(price_mana_parcel)) AS avg_price_mana_parcel
 FROM 		        sales
+WHERE           price_usd_parcel > 50
 GROUP BY 	      1
 ORDER BY  	    1 asc
